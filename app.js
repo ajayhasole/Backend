4b56bd103e13830ab595f53a2aa5ccbfb946571e
Soham Ingale <sohamingale2000@gmail.com>
2024-05-16 01:31:19 +0530

    backen-admin-procedure-complete

---
+++ app.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import adminRouter from './routes/admin-routes.js'
+
+
+
+
+dotenv.config();
+
+const PORT = 6800;
+const app = express();
+app.use(express.json());
+
+app.use('/admin', adminRouter);
+
+
+
+
+
+mongoose.connect(
+    `mongodb+srv://sohamingale2000:${process.env.MONGODB_PASSWORD}@cluster.aa3kduh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster`
+).then(()=>{
+    app.listen(`${PORT}`,()=>{
+        console.log(`Connected to DB and Server ${PORT} is running`);
+    })
+}).catch(error=>{
+    console.log(error);
+})
+
+
+
